5979564aaa812787fa9132f96ff99096583afea1
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-10-05 04:54:21 +0300

    import CommonModule

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Task } from '../../Task';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css',
 })
