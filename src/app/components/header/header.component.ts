008ffeb409903663b739d65fa8b1803e6ceb6191
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-10-10 00:43:42 +0300

    feat: update toggleAddTask & subscribe to toggle subject

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -10,10 +12,18 @@ import { ButtonComponent } from '../button/button.component';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Manager';
-  constructor() {}
+  showAddTask: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
   ngOnInit(): void {}
 
   toggleAddTask() {
+    this.uiService.toggleAddTask();
     console.log('toggle');
   }
 }
